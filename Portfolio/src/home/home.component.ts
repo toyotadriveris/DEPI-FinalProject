e651e7af88a4d2a149c44774af10f7ebd29b7dfd
toyotadriver <polaemil@gmail.com>
2024-10-04 23:51:16 +0300

    "fixed postions of elements hello and el zero text."

--- Portfolio/src/home/home.component.ts
+++ Portfolio/src/home/home.component.ts
@@ -1,24 +1,54 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
   eleTarget: any = '';
+  elementPosX: any;
+  elZeroTargetPos: any;
 
-  getPostion () {
-    let textContainer = document.querySelector('.txtContainer')
-    let elementref = document.querySelector('.landingImg');
-    let elementTop = elementref?.getBoundingClientRect().height;
+  constructor(){}
+  ngOnInit() {
+
+    let ElementRef = document.querySelector('.osamaImg')
+    let elementTarget = document.querySelector('.txtContainer');
+    let targetX:any = ElementRef?.getBoundingClientRect().width;
+    let screenSize = window.innerWidth;
+
+    this.elementPosX = -(targetX * 0.23);
+
+    let elZero = document.querySelector(".elzero");
+
+    this.elZeroTargetPos = targetX * 0.43;
     
-    this.eleTarget = elementTop + '50'
-    console.log(this.eleTarget);
+  }
+  
+  getPostion () {
+    let ElementRef = document.querySelector('.osamaImg')
+    let elementTarget = document.querySelector('.txtContainer');
+    let targetX:any = ElementRef?.getBoundingClientRect().width;
+    let screenSize = window.innerWidth;
+
+    if (screenSize >= 1840) {
+      this.elementPosX = -(targetX * 0.23);
+    } else {
+      this.elementPosX = -(targetX * 0.2);
+    }
+
+    let elZero = document.querySelector(".elzero");
+
+    this.elZeroTargetPos = targetX * 0.43;
+    
 
   }
+
+
 
 }
